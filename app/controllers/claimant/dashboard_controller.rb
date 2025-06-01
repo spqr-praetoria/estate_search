cbde2951fb6561fe6d92d6fcc1ed4590d68a3ff2
Benjamin Fowl <b.fowl87@gmail.com>
2025-06-01 09:29:22 +0100

    feat: setup basic layout for claimant user

--- app/controllers/claimant/dashboard_controller.rb
+++ app/controllers/claimant/dashboard_controller.rb
@@ -1,5 +1,7 @@
 class Claimant::DashboardController < Claimant::ApplicationController
   def index
     authorize! :dashboard
+    @title = "Questions"
+    @questions = current_user.questions.eager_load(:answers).order(created_at: :desc)
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def page_to(path, **options, &block)
+    if current_page?(path)
+      content_tag(:a, class: options[:class]) do
+        block.call
+      end
+    else
+      link_to(path, **options, &block)
+    end
+  end
 end
