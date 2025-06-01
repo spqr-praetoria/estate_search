caf95bfb6156106d65c3533f16de1ae6d713c6b0
Ben <b.fowl87@gmail.com>
2025-06-01 10:53:45 +0100

    feat: basic setup of lawyer with questions dashboard

--- app/controllers/lawyer/dashboard_controller.rb
+++ app/controllers/lawyer/dashboard_controller.rb
@@ -1,5 +1,9 @@
 class Lawyer::DashboardController < Lawyer::ApplicationController
   def index
     authorize! :dashboard
+
+    # Query: Can a lawyer submit multiple answers to the same question
+    # I've made it so that it's 1 question per lawyer
+    @questions = Question.not_closed.not_answered_by(current_user).order(created_at: :desc)
   end
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -3,9 +3,33 @@ class Question < ApplicationRecord
 
   has_many :answers, dependent: :destroy
 
+  scope :not_closed, -> { where.not(status: :closed) }
+  scope :not_answered_by, ->(lawyer) {
+    left_joins(:answers)
+      .where("answers.lawyer_id IS NULL OR answers.lawyer_id != ?", lawyer.id)
+      .distinct
+  }
+
   validates :title, :body, presence: true
+  validates :status, inclusion: { in: %w[open answered closed] }
+  validates :category, inclusion: { in: %w[general missing_assets wills_and_probate legal_heirship] }
 
   has_rich_text :body
+
+  after_create_commit do
+    broadcast_prepend_to "questions", partial: "claimant/questions/question"
+    broadcast_prepend_to "lawyer_questions", partial: "lawyer/questions/question"
+  end
+
+  after_update_commit do
+    broadcast_replace_to "questions", partial: "claimant/questions/question"
+    broadcast_replace_to "lawyer_questions", partial: "lawyer/questions/question"
+  end
+
+  after_destroy_commit do
+    broadcast_remove_to "questions", partial: "claimant/questions/question"
+    broadcast_remove_to "lawyer_questions", partial: "lawyer/questions/question"
+  end
 
   enum :category, {
     general: 0,
